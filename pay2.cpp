45d739aecdc3bb9938c4cfde6150307973caae8a
sthvjay <47376382+sthvjay@users.noreply.github.com>
2019-04-07 17:41:44 -0700

    added the result of highestpaid at the end of the pay2

--- pay2.cpp
+++ pay2.cpp
@@ -5,25 +5,27 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <iterator>
 
 using namespace std;
 
-void readData(vector<Person> & employees);
+void readData(vector<Person> &employees);
 void getCompanies (vector<Person> &employees, vector<string> &companyName);
-// void printHighestPaid (vector<Person> &employees);
-// void seperateAndSave (vector<person> &employees, vector<string> &companyName);
+void printHighestPaid (vector<Person> &employees);
+void separateAndSave (vector<Person> &employees, vector<string> &companyName);
 
 int main(){
   vector<Person> employees;
-  vector<string> companyName;
+  vector<string> companyNames;
   readData(employees);
-  getCompanies (employees,companyName);
-  // printHighestPaid(employees);
-  // seperateAndSave(employees,companyName);
+  getCompanies (employees,companyNames);
+  printHighestPaid(employees);
+  separateAndSave(employees,companyNames);
+  cin.get();
   return 0;
   }
 //------------------------------------------------------------------------------------readData
-void readData(vector<Person> &employees)
+void readData(vector<Person>&employees)
 {
 string fName;
 string lName;
@@ -34,65 +36,81 @@ float rate;
 Person a;
 fstream inputfile;
 inputfile.open("input2.txt");
-while (!inputfile.eof){
- inputfile>>fName>>lName>>id>>cName>>hours>>rate
-  a.setFirstName(fName);
-  a.setLastName(lName);
-  a.setEmployeeId (id);
-  a.setHoursWorked (hours);
-  a.setPayRate (rate);
+if(inputfile.is_open())
+  {
+      while (inputfile>>fName>>lName>>id>>cName>>hours>>rate)
+      {
+        a.setFirstName(fName);
+        a.setLastName(lName);
+        a.setEmployeeId (id);
+        a.setHoursWorked (hours);
+        a.setPayRate (rate);
 
-  employees.push_back(a);
-}
-infile.close();
+        employees.push_back(a);
+      }
+  }
+  inputfile.close();
 }
 
 
 // ---------------------------------------------------------------company name
-void getCompanies (vector<Person> &employees, vector<string> &companyName){
-string comName;
-for(int i= 0; i<employees.size();i++){
-  comName = employees[i].getCompanyName();
-
-  if(count (companyName.begin(), companyName.end(),comName) == 0){
-    companyName.push_back(comName);
+void getCompanies (vector<Person> &employees, vector<string> &companyName)
+{
+  string comName;
+  for(int i= 0; i<employees.size(); i++)
+  {
+      comName = employees.at(i).getCompanyName();
+          if(count (companyName.begin(), companyName.end(),comName) == 0)
+          {
+              companyName.push_back(comName);
+          }
   }
-}
 }
 
 //----------------------------------------------------------------- highestpaid
-// void printHighestPaid (vector<Person> &employees){
-// float highest = 0.0;
-// int count ;
-//   for (int i =0; i<employees.size(); i++) {
-//     if (employees.at(i).totalPay()> highest)
-//     highest = employees.at(i).totalPay();
-//     count = i;
-//     }
-//     cout << "Highest paid : "<< employees.at(count).fullName<< endl;
-//       cout << "Employee ID : "<< employees.at(count).getEmployeeId<< endl;
-//         cout << "Employer : "<< employees.at(count).getCompanyName<<endl;
-//           cout << "Total Pay : $"<< employees.at(count).totalPay > endl;
-//
-// }
-//
-// //---------------------------------------------------------------------
-// void seperateAndSave(vector<Person>& employees, vector<string>&companyName) {
-//
-// float total= 0.0;
-// for(int i= 0; i< companyName.size(); i++){
-// ofstream outputfile;
-// string filename = companyName.at(i)+".txt";
-// outputfile.open(filename + ".txt");
-// outputfile<< "Company name:"<<filename<<endl;
-//     for(int j =0; j<employees.size()-1; j++){
-//      if(filename == employees.at(j).getCompanyName()){
-//        total += employees.at(j).totalPay();
-//        outputfile<<setw(10)<<left<<employees.at((j).fullName()<<"  "<<
-//         employees.at(j).getEmployeeId()<< "  "<< employees.at(j).getCompanyName() << " "<< "$" <<employees.at(j).totalPay()<<endl;
-// }
-// }
-//        outputfile<< "Total :" << " " <<" $"<< total<<endl;
-//     outputfile.close();
-// }
-// }
+void printHighestPaid(vector<Person> &employees)
+{
+  float highest;
+  int count;
+  for (int i = 0; i<employees.size(); i++)
+  {
+    if (employees.at(i).totalPay()> highest)
+    {
+          highest = employees.at(i).totalPay();
+          count = i;
+    }
+  }
+    cout << "Highest paid : " << employees.at(count).fullName() << endl;
+    cout << "Employee ID : "<< employees.at(count).getEmployeeId()<< endl;
+    cout << "Employer : "<< employees.at(count).getCompanyName()<<endl;
+    cout << "Total Pay : $"<<setprecision(2)<<fixed<< employees.at(count).totalPay()<< endl;
+}
+
+//---------------------------------------------------------------------
+void separateAndSave(vector<Person>& employees, vector<string>&companyName)
+{
+float total;
+for(int i= 0; i< companyName.size(); i++)
+  {
+      ofstream output;
+      string comp= companyName[i]+ ".txt";
+      output.open(comp);
+      output << "----------------------------------------------------------"<< endl;
+        for(int j =0; j<employees.size()-1; j++)
+        {
+         if(employees.at(j).getCompanyName()== companyName.at(i))
+            {
+                total += employees.at(j).totalPay();
+                output<<setw(10)<<left<<employees[j].fullName()<<"  "<<
+                employees[j].getEmployeeId()<< "  "<< employees[j].getCompanyName() << " "<< "$" <<employees[j].totalPay()<<endl;
+            }
+        }
+       output<<fixed<< setprecision(2)<<"Total " << " " <<" $"<< total << endl;
+       output.close();
+  }
+}
+
+// Highest paid : Ryan Ward
+// Employee ID : 150
+// Employer :
+// Total Pay : $3281.00
